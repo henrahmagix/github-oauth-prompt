b2ae4e24e542086bb8eab43968db2994c4c8f697
henry <henry@incuna.com>
2014-06-24 22:44:52 +0100

    Describe code in README and tests.

--- README.md
+++ README.md
@@ -7,9 +7,50 @@ Install the module with: `npm install github-oauth`
 
 ```javascript
 var oauth = require('github-oauth');
-// Prompt for user, pass, and two-factor auth code. Return a token.
-// `name` is required.
-var token = oauth({name: 'my-token-name'});
+
+// Prompt for username, password, two-factor auth code if required,
+// and return a GitHub token with basic scope.
+var moonriseToken = oauth({
+    name: 'moonrise-kingdom'
+});
+
+// Prompt for a token for read/write access to public repositories
+// and organisations and write access to Gists.
+var rushmoreToken = oauth({
+    name: 'rushmore',
+    scopes: ['public_repo', 'gist']
+});
+
+// Set prompt messages (mild customisation).
+var aquaticToken = oauth({
+    name: 'the-life-aquatic',
+    prompt: {
+        username: 'Enter username:',
+        password: 'Enter password:',
+        code: 'Enter two-factor authorisation code'
+    }
+});
+
+// Skip prompt (spicy customisation).
+var username = promptForUsername();
+var password = promptForPassword();
+var code = null;
+// First test to see if a code is required.
+var hasTwoFactorAuth = oauth.requiresCode({
+    username: username,
+    password: password
+});
+if (hasTwoFactorAuth) {
+    // Ask for a code.
+    code = promptForCode();
+}
+// Get a token with a two-factor authentication code.
+var royalToken = oauth({
+    name: 'the-royal-tenenbaums'
+    username: username,
+    password: password,
+    code: code
+});
 ```
 
 ## Documentation
--- test/github-oauth_test.js
+++ test/github-oauth_test.js
@@ -2,6 +2,11 @@
 
 var oauth = require('../lib/github-oauth.js');
 
+// Mock the GitHub API.
+var nock = require('nock');
+var api = nock('https://api.github.com').log(console.log);
+api.get('/').reply(200);
+
 /*
     ======== A Handy Little Mocha Reference ========
     https://github.com/visionmedia/mocha/
@@ -28,9 +33,46 @@ var oauth = require('../lib/github-oauth.js');
 
 var assert = require('assert');
 
-suite('OauthTest', function(){
-    test('#default()', function(done){
-        assert.equal(oauth(), 'Token');
-        done();
+describe('Oauth', function () {
+
+    beforeEach(function () {
+        nock.cleanAll();
     });
+
+    it("should return 'Token' string when called", function () {
+        assert.equal(oauth(), 'Token');
+    });
+
+    // Init.
+    it('should error if required field "name" not given');
+    it('should error if field "scopes" cannot be made into an array');
+
+    // Normal use: asks for details.
+    it('should not error on username input');
+    it('should error and ask for input again on no username input');
+    it('should not error on password input');
+    it('should error and ask for input again on no password input');
+    it('should continue after password input when no 2FA code required');
+    it('should ask for a 2FA code when required');
+    it('should not error on 2FA code input');
+    it('should error and ask for input again on no 2FA code input');
+
+    // Static use: accepts details.
+    it('should error when username is not passed to static call');
+    it('should error when password is not passed to static call');
+    it('should return a 2FA code request from a static call if required');
+    it('should error when 2FA code is required and not given for a static call');
+
+    // Authentication test.
+    it('should error an authentication test with bad credentials when 2FA not required');
+    it('should error an authentication test with bad credentials when 2FA required');
+    it('should succeed an authentication test with good basic credentials');
+    it('should succeed an authentication test with good 2FA credentials');
+
+    // Token creation.
+    it('should error when it cannot connect to the api');
+    it('should explain when rate limit remaining is 0');
+    it('should create and return a new token');
+    it('should get and return an existing token by name');
+
 });
